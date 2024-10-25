cf5f31f3bfd98107e44b44f4fc477bfa6412f296
yangerhang <yangerhang@wps.cn>
2024-10-25 18:57:05 +0800

    add parser

--- src/extension.ts
+++ src/extension.ts
@@ -1,22 +1,300 @@
 import * as vscode from 'vscode';
 
-function extractClassName(text: string) {
-	let patts: [patt: RegExp, classNameGroupIdx: number][] =
-		[
-			/* */[/^(struct|class)\s+([a-zA-Z0-9_<>]+)?\s*([a-zA-Z0-9_<>]+)\s*([:,]\s*(public|private|protected)?\s*[a-zA-Z0-9_<>]+\s*)*\s*{$/g, 2],
-			//                       aaaaaaaaaaaaaaaaaa   nnnnnnnnnnnnnnn     bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
-			// a: 'attribute' for the class. For example, 'MY_EXPORT' as in 'class MY_EXPORT Car'
-			// n: class name
-			// b: base classes
-		];
-	for (let [patt, classNameGroupIdx] of patts) {
-		let match = patt.exec(text)
-		if (!match)
-			continue
-		let className = match[classNameGroupIdx]
-		return className
+export enum TokenType {
+	Unknown,
+	Space,
+	ClassKeyword,
+	PublicKeyword,
+	Ident,
+	LParen,
+	RParen,
+	LBrace,
+	RBrace,
+	SemiColumn,
+	ColumnColumn,
+	Column,
+	LAngleBracket,
+	RAngleBracket,
+	Comma,
+}
+
+export class Token {
+	text: string;
+	type: TokenType;
+	constructor(text: string, type: TokenType) {
+		this.text = text;
+		this.type = type;
 	}
-	return null
+}
+
+export class Tokenizer {
+	stack: string[];
+
+	constructor(text: string) {
+		this.stack = [];
+		this.stack.push(text);
+	}
+
+	get text() {
+		return this.stack[this.stack.length - 1]
+	}
+
+	set text(value: string) {
+		this.stack[this.stack.length - 1] = value
+	}
+
+	public save() {
+		this.stack.push(this.text)
+	}
+
+	public restore() {
+		this.stack.pop()
+	}
+
+	public prepend(value: string) {
+		this.text = value + this.text;
+	}
+
+	public next() {
+		let m;
+		if (this.text.length == 0) {
+			return undefined
+		} else if (m = this.text.match(/^\s+/g)) {
+			return this.sliceToken(m[0].length, TokenType.Space);
+		} else if (m = this.text.match(/^(class|struct)/g)) {
+			return this.sliceToken(m[0].length, TokenType.ClassKeyword);
+		} else if (m = this.text.match(/^(public|private|protected)/g)) {
+			return this.sliceToken(m[0].length, TokenType.PublicKeyword);
+		} else if (m = this.text.match(/^\w+/g)) {
+			return this.sliceToken(m[0].length, TokenType.Ident);
+		} else if (m = this.text.match(/^\(/g)) {
+			return this.sliceToken(m[0].length, TokenType.LParen);
+		} else if (m = this.text.match(/^\)/g)) {
+			return this.sliceToken(m[0].length, TokenType.RParen);
+		} else if (m = this.text.match(/^{/g)) {
+			return this.sliceToken(m[0].length, TokenType.LBrace);
+		} else if (m = this.text.match(/^}/g)) {
+			return this.sliceToken(m[0].length, TokenType.RBrace);
+		} else if (m = this.text.match(/^;/g)) {
+			return this.sliceToken(m[0].length, TokenType.SemiColumn);
+		} else if (m = this.text.match(/^::/g)) {
+			return this.sliceToken(m[0].length, TokenType.ColumnColumn);
+		} else if (m = this.text.match(/^:/g)) {
+			return this.sliceToken(m[0].length, TokenType.Column);
+		} else if (m = this.text.match(/^,/g)) {
+			return this.sliceToken(m[0].length, TokenType.Comma);
+		} else if (m = this.text.match(/^\</g)) {
+			return this.sliceToken(m[0].length, TokenType.LAngleBracket);
+		} else if (m = this.text.match(/^\>/g)) {
+			return this.sliceToken(m[0].length, TokenType.RAngleBracket);
+		}
+		return this.sliceToken(1, TokenType.Unknown);
+	}
+
+	private sliceToken(len: number, tt: TokenType) {
+		const token = new Token(this.text.slice(0, len), tt);
+		this.text = this.text.slice(len, undefined);
+		return token;
+	}
+}
+
+// Name of a class or an instance of class template.
+// e.g. 'MyClass', 'MyTemplatedClass<int>'
+type ClassName = { name: string, args: ClassName[] };
+// Base class declaration, part of the class declaration. e.g. 'public MyBase', 'private MyBase', 'MyBase'
+//     access: 'public' 'private' 'protected' or 'default' (no access modifier)
+type BaseDecl = { access: string, className: ClassName };
+// Class declaration up until left curly brace. e.g. 'class MyClass {'
+type ClassDecl = { className: string, attribute: string, bases: BaseDecl[] };
+
+export class Parser {
+
+	tokenizer: Tokenizer;
+
+	constructor(tokenizer: Tokenizer) {
+		this.tokenizer = tokenizer;
+	}
+
+	public parse(): ClassDecl | undefined {
+		return this.parseClassDecl()
+	}
+
+	private nextGoodToken(): Token | undefined {
+		let token;
+		while (token = this.tokenizer.next()) {
+			if (token.type != TokenType.Space)
+				return token;
+		}
+		return undefined;
+	}
+	// parses:
+	//     class MyClass
+	//     class EXPORT_STUFF MyClass
+	private parseNameAndAttrInClassDecl() {
+		const classKw = this.nextGoodToken()
+		if (!classKw || classKw.type != TokenType.ClassKeyword)
+			return undefined;
+		const token1 = this.nextGoodToken()
+		if (!token1 || token1.type != TokenType.Ident)
+			return undefined;
+		const token2 = this.nextGoodToken()
+		if (!token2)
+			return { name: token1.text, attribute: "" }
+		if (token2.type != TokenType.Ident) {
+			this.tokenizer.prepend(token2.text);
+			return { name: token1.text, attribute: "" }
+		}
+		return { name: token2.text, attribute: token1.text }
+	}
+
+	// parses:
+	//     class MyClass {
+	//     class EXPORT_STUFF MyClass {
+	//     class MyClass : <base-list> {
+	private parseClassDecl(): ClassDecl | undefined {
+		const nameAndAttr = this.parseNameAndAttrInClassDecl();
+		if (!nameAndAttr)
+			return undefined;
+		const token = this.nextGoodToken()
+		if (!token)
+			return undefined;
+		// class A : ... {
+		//         \__ token
+		if (token.type == TokenType.Column) {
+			let bases;
+			if (bases = this.parseBaseList()) {
+				const brace = this.nextGoodToken()
+				if (!brace || brace.type != TokenType.LBrace)
+					return undefined;
+				return {
+					className: nameAndAttr.name,
+					attribute: nameAndAttr.attribute,
+					bases: bases,
+				};
+			}
+			return undefined;
+		}
+		// class A {
+		//         \___ token
+		else if (token.type == TokenType.LBrace) {
+			return {
+				className: nameAndAttr.name,
+				attribute: nameAndAttr.attribute,
+				bases: [],
+			};
+		}
+		else
+			return undefined;
+	}
+
+	private parseCommaSeparatedList<Item>(parseItem: () => Item | undefined): Item[] | undefined {
+		let items: Item[] = []
+		let count = 0;
+		while (count < 99) {
+			const item = parseItem();
+			if (!item)
+				return undefined;
+			items.push(item);
+			let maybeComma = this.nextGoodToken();
+			if (!maybeComma)
+				return items
+			if (maybeComma.type != TokenType.Comma) {
+				this.tokenizer.prepend(maybeComma.text)
+				return items
+			}
+			count++;
+		}
+		return undefined;
+	}
+
+	// parses:
+	//     public Base1, public Base2, ... , public BaseN
+	private parseBaseList(): BaseDecl[] | undefined {
+		const items = this.parseCommaSeparatedList(() => {
+			return this.parseBase();
+		});
+		return items;
+	}
+
+	// parses:
+	//     public Base
+	//     Base
+	//     public TemplatedBase<int>
+	private parseBase(): BaseDecl | undefined {
+		const publicKwOrIdent = this.nextGoodToken();
+		if (!publicKwOrIdent)
+			return undefined;
+		switch (publicKwOrIdent.type) {
+			case TokenType.PublicKeyword: {
+				let r;
+				if (r = this.parseClassName()) {
+					return {
+						access: publicKwOrIdent.text,
+						className: r
+					};
+				}
+			}
+				break;
+			case TokenType.Ident: {
+				this.tokenizer.prepend(publicKwOrIdent.text);
+				let r;
+				if (r = this.parseClassName()) {
+					return {
+						access: "default",
+						className: r
+					};
+				}
+			}
+				break;
+			default:
+				return undefined;
+		}
+		return undefined;
+	}
+
+	// parses:
+	//     MyClass
+	//     MyClassTemplate<int>
+	//     MyClassTemplate<int, double, ... , float, WTF<int> >
+	private parseClassName(): ClassName | undefined {
+		const ident = this.nextGoodToken();
+		if (!ident || ident.type != TokenType.Ident)
+			return undefined;
+		const maybeLAngleBracket = this.nextGoodToken();
+		if (!maybeLAngleBracket)
+			return { name: ident.text, args: [] }
+		if (maybeLAngleBracket.type != TokenType.LAngleBracket) {
+			this.tokenizer.prepend(maybeLAngleBracket.text);
+			return { name: ident.text, args: [] }
+		}
+		const args = this.parseTemplateArgList();
+		if (!args)
+			return undefined;
+		const rAngleBracket = this.nextGoodToken();
+		if (!rAngleBracket || rAngleBracket.type != TokenType.RAngleBracket)
+			return undefined;
+		return { name: ident.text, args: args }
+	}
+
+	// parses:
+	//     int, double, ... , float, WTF<int>
+	private parseTemplateArgList(): ClassName[] | undefined {
+		const args = this.parseCommaSeparatedList(() => {
+			return this.parseClassName();
+		});
+		return args;
+	}
+}
+
+function extractClassName(text: string) {
+	const tokenizer = new Tokenizer(text);
+	const parser = new Parser(tokenizer);
+	let r;
+	if (r = parser.parse())
+	{
+		return r.className;
+	}
+	return undefined;
 }
 
 export function activate(context: vscode.ExtensionContext) {
@@ -29,15 +307,17 @@ export function activate(context: vscode.ExtensionContext) {
 		const position = editor.selection.active
 		const document = editor.document
 		const textUpToCursor = document.getText(new vscode.Range(new vscode.Position(0, 0), position))
-		const pass1Matches = textUpToCursor.match(/class(.|\s)+?{/g)
+		// Broad matching. Find substrings starting with 'class' ending with '{'.
+		const pass1Matches = textUpToCursor.match(/(struct|class)[^;]+?{/g)
 		if (!pass1Matches) {
 			vscode.window.showInformationMessage(`Class not found in ${textUpToCursor}`)
 			return;
 		}
 		for (let pass1Match of pass1Matches) {
-			if (extractClassName(pass1Match) !== null) {
-				vscode.window.showInformationMessage(pass1Match)
-			}
+			let name = extractClassName(pass1Match)
+			if (!name)
+				continue
+			vscode.window.showInformationMessage(`name = ${name}, excerpt = ${pass1Match}`)
 		}
 		vscode.window.showInformationMessage("done")
 	});
--- src/test/extension.test.ts
+++ src/test/extension.test.ts
@@ -1,15 +1,91 @@
 import * as assert from 'assert';
 
-// You can import and use all API from the 'vscode' module
-// as well as import your extension to test it
 import * as vscode from 'vscode';
-// import * as myExtension from '../../extension';
+import * as x from '../extension';
 
-suite('Extension Test Suite', () => {
+suite('cpp-def-paster', () => {
 	vscode.window.showInformationMessage('Start all tests.');
+	test('tokenizer 1', () => {
+		const tokenizer = new x.Tokenizer("class   FUNNY_EXPORT Class1 : public FOO<int, double>, Bar {} ;")
+		let token;
+		const expected: x.Token[] = [
+			new x.Token("class", x.TokenType.ClassKeyword),
+			new x.Token("FUNNY_EXPORT", x.TokenType.Ident),
+			new x.Token("Class1", x.TokenType.Ident),
+			new x.Token(":", x.TokenType.Column),
+			new x.Token("public", x.TokenType.PublicKeyword),
+			new x.Token("FOO", x.TokenType.Ident),
+			new x.Token("<", x.TokenType.LAngleBracket),
+			new x.Token("int", x.TokenType.Ident),
+			new x.Token(",", x.TokenType.Comma),
+			new x.Token("double", x.TokenType.Ident),
+			new x.Token(">", x.TokenType.RAngleBracket),
+			new x.Token(",", x.TokenType.Comma),
+			new x.Token("Bar", x.TokenType.Ident),
+			new x.Token("{", x.TokenType.LBrace),
+			new x.Token("}", x.TokenType.RBrace),
+			new x.Token(";", x.TokenType.SemiColumn),
+		]
+		let actual: x.Token[] = []
+		while (token = tokenizer.next()) {
+			if (token.type != x.TokenType.Space)
+				actual.push(token)
+		}
 
-	test('Sample test', () => {
-		assert.strictEqual(-1, [1, 2, 3].indexOf(5));
-		assert.strictEqual(-1, [1, 2, 3].indexOf(0));
+		for (let i = 0; i < Math.min(actual.length, expected.length); i++) {
+			const expectedToken = expected[i];
+			const actualToken = actual[i];
+			assert.strictEqual(actualToken.text, expectedToken.text, `token text mismatch, index = ${i}`)
+			assert.strictEqual(actualToken.type, expectedToken.type, `token type mismatch, index = ${i}`)
+		}
+		assert.strictEqual(actual.length, expected.length)
+	});
+	test('parser 1', () => {
+		const tokenizer = new x.Tokenizer("class Happy : Base1<int> {};")
+		const parser = new x.Parser(tokenizer);
+		const result = parser.parse()
+		assert.strictEqual(JSON.stringify(result), '{"className":"Happy","attribute":"","bases":[{"access":"default","className":{"name":"Base1","args":[{"name":"int","args":[]}]}}]}', `bad parse`)
+	});
+	test('parser 2', () => {
+		const tokenizer = new x.Tokenizer("class Happy {};")
+		const parser = new x.Parser(tokenizer);
+		const result = parser.parse()
+		assert.strictEqual(JSON.stringify(result), '{"className":"Happy","attribute":"","bases":[]}', `bad parse`)
+	});
+	test('parser 3', () => {
+		const tokenizer = new x.Tokenizer("class Happy:public Base{")
+		const parser = new x.Parser(tokenizer);
+		const result = parser.parse()
+		assert.strictEqual(JSON.stringify(result), '{"className":"Happy","attribute":"","bases":[{"access":"public","className":{"name":"Base","args":[]}}]}', `bad parse`)
+	});
+	test('parser 4', () => {
+		const tokenizer = new x.Tokenizer("class Happy:Nest1<Nest2<double>>{")
+		const parser = new x.Parser(tokenizer);
+		const result = parser.parse()
+		assert.strictEqual(JSON.stringify(result), '{"className":"Happy","attribute":"","bases":[{"access":"default","className":{"name":"Nest1","args":[{"name":"Nest2","args":[{"name":"double","args":[]}]}]}}]}', `bad parse`)
+	});
+	test('parser 5', () => {
+		const tokenizer = new x.Tokenizer("class EXPORT Happy : Base1<int> {};")
+		const parser = new x.Parser(tokenizer);
+		const result = parser.parse()
+		assert.strictEqual(JSON.stringify(result), '{"className":"Happy","attribute":"EXPORT","bases":[{"access":"default","className":{"name":"Base1","args":[{"name":"int","args":[]}]}}]}', `bad parse`)
+	});
+	test('parser 6', () => {
+		const tokenizer = new x.Tokenizer("class EXPORT Happy {};")
+		const parser = new x.Parser(tokenizer);
+		const result = parser.parse()
+		assert.strictEqual(JSON.stringify(result), '{"className":"Happy","attribute":"EXPORT","bases":[]}', `bad parse`)
+	});
+	test('parser 7', () => {
+		const tokenizer = new x.Tokenizer("class  EXPORT  Happy:public Base{")
+		const parser = new x.Parser(tokenizer);
+		const result = parser.parse()
+		assert.strictEqual(JSON.stringify(result), '{"className":"Happy","attribute":"EXPORT","bases":[{"access":"public","className":{"name":"Base","args":[]}}]}', `bad parse`)
+	});
+	test('parser 8', () => {
+		const tokenizer = new x.Tokenizer("class EXPORT Happy:Nest1<Nest2<double>>{")
+		const parser = new x.Parser(tokenizer);
+		const result = parser.parse()
+		assert.strictEqual(JSON.stringify(result), '{"className":"Happy","attribute":"EXPORT","bases":[{"access":"default","className":{"name":"Nest1","args":[{"name":"Nest2","args":[{"name":"double","args":[]}]}]}}]}', `bad parse`)
 	});
 });
